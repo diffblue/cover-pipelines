618173a49ccdc182d71596c696025d67c42d4f38
Jeannie Moulton <jeannie.moulton@diffblue.com>
2022-01-11 16:32:06 +0000

    Add multi-module jenkins example.

---
+++ jenkins-github-maven/scripts/server/diffblueRunExistingTests.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# dcover location
+RELEASE_URL="$1"
+# project modules - note this must come last as it is var args
+MODULES="$2"
+
+# Jenkins runs this script from the workspace, not where the script is stored
+. ./.jenkins/scripts/common.sh
+if [ $? -ne 0 ]
+then
+  echo "[DIFFBLUE CI PIPELINE] Could not load the common file from $PWD"
+  exit 1
+fi
+
+# Extracting constants from common.sh
+TEST_LOCATION="$(diffblueTestLocation)"
+DCOVER_SCRIPT_LOCATION="$(getDcoverScriptLocation)"
+echoDiffblue "Diffblue test location: $TEST_LOCATION, DCover script location: $DCOVER_SCRIPT_LOCATION"
+
+# The project is built here to keep this simple. This could potentially be improved by building and exporting artifacts.
+echoDiffblue "Build project"
+eval "$(commandToBuildProject)"
+
+echoDiffblue "Get dcover"
+getDcover "$RELEASE_URL"
+checkSuccess $?
+
+echoDiffblue "Remove non-compiling tests in each module"
+for MODULE in ${MODULES[@]}
+do
+  echoDiffblue "Compile tests"
+  eval "$(commandToCompileTestsForSingleModule)"
+  echoDiffblue "Removing non-compiling tests in $MODULE"
+	"$DCOVER_SCRIPT_LOCATION" clean -d "$TEST_LOCATION" --working-directory "$MODULE"
+  checkSuccess $?
+done
+
+# This must only fail at the end after all tests are run to be the most useful
+echoDiffblue "Running Diffblue tests in base branch against your code changes"
+eval "$(commandToRunAllDiffblueTestsFailingAtTheEnd)"
