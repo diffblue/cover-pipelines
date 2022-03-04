6e2661d03a91d41ebcb8bd017b7059b3af68dc9b
Jeannie Moulton <jeannie.moulton@diffblue.com>
2022-03-04 06:05:25 +0000

    Align with internal scripts.

--- jenkins-github-maven/scripts/pipeline/updateComment.sh
+++ jenkins-github-maven/scripts/pipeline/updateComment.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# Copyright 2021-2022 Diffblue Limited. All Rights Reserved.
-# Unpublished proprietary source code.
-# Use is governed by https://docs.diffblue.com/licenses/eula
-
 # This script is responsible for the logic and API interaction for the remote host comment
 # This script example uses the Github API. Similar API calls will need to be substituted for other remote hosts.
+
+# The script generates a comment that looks like, for example:
+# <h2>:coffee: Diffblue CI is running existing Diffblue tests against your code changes.</h2><ul>
+# <li>:white_check_mark: Run existing Diffblue tests against your code changes.</li>
+# <li>:white_check_mark: Update Diffblue tests to reflect your code changes.</li></ul>
+# The text in each line depends the result of the existing-diffblue-tests and update-diffblue-tests job
+# which are reflected in the variables EXISTING_TESTS_STATUS and EXISTING_TESTS_STATUS
 
 TOKEN="$1" # Token for API interaction with remote host
 PR_NUMBER=$2
@@ -29,99 +32,94 @@ echoDiffblue "arguments (7): TOKEN, $PR_NUMBER, $EXISTING_TESTS_STATUS, $EXISTIN
 # This is used to tag the comment with a unique identifier to find it and delete/update, etc
 DIFFBLUE_UNIQUE_COMMENT_ID="diffblue-update-tests-comment-id"
 
-
-####### Comment logic #######
-
-COMMENT_ANALYSIS_RUNNING_EXISTING="<h2>:coffee: Diffblue CI is running existing Diffblue tests against your code changes.</h2><ul>"
-COMMENT_ANALYSIS_UPDATING="<h2>:coffee: Diffblue CI is analysing your code changes to update Diffblue tests.</h2><ul>"
-COMMENT_ANALYSIS_COMPLETE="<h2>:heavy_check_mark: Diffblue CI has analysed your code changes and is ready for your review.</h2><ul>"
-COMMENT_EXISTING_TODO="<li>:white_check_mark: Run existing Diffblue tests against your code changes.</li>"
-COMMENT_UPDATED_TODO="<li>:white_check_mark: Update Diffblue tests to reflect your code changes.</li>"
-COMMENT_EXISTING_DONE_FAILED="<li>:question: Existing Diffblue tests failed against your code changes. See the test report <a href=\"${EXISTING_TESTS_BUILD_URL}testReport/\">here</a> to check if these are expected failures or regressions. Note that any non-compiling tests have been removed and will not appear in the report.</li>"
-COMMENT_EXISTING_DONE_PASSED="<li>:heavy_check_mark: Existing Diffblue tests passed against your code changes. See the test report <a href=\"${EXISTING_TESTS_BUILD_URL}testReport/\">here</a>. Note that any non-compiling tests have been removed and will not appear in the report.</li>"
-COMMENT_UPDATED_DONE_TESTS="<li>:heavy_check_mark: Diffblue has added commits to update the tests. Check that these reflect the intended behaviour of your code change before merging them. <a href=\"${UPDATING_TESTS_BUILD_URL}console/\">Build log</a></li>"
-COMMENT_UPDATED_DONE_NO_TESTS="<li>:heavy_check_mark: Diffblue did not update any tests. <a href=\"${UPDATING_TESTS_BUILD_URL}console/\">Build log</a></li>"
-COMMENT_UPDATED_DONE_FAILURE="<li>:x: Something went wrong. <a href=\"${UPDATING_TESTS_BUILD_URL}console/\">Build log</a></li>"
-
 GITHUB_ORG="$(githubOrg)"
 GITHUB_REPO="$(githubRepo)"
 echoDiffblue "Commenting for org $GITHUB_ORG and repo $GITHUB_REPO"
 
-# Update tests status
 remoteHostAuthentication "$SSH_KEY"
 git fetch origin $CHANGE_TARGET -q
 git fetch origin $CHANGE_BRANCH -q
-GIT_AUTHOR_LIST=$(git log origin/$CHANGE_TARGET..origin/$CHANGE_BRANCH --pretty=%cn)
-# echoDiffblue "Author list: $GIT_AUTHOR_LIST"
-if [ "$UPDATING_TESTS_STATUS" == "COMPLETE" ]
-then
-  if [[ $GIT_AUTHOR_LIST = *"$(diffblueBotName)"* ]]
-  then
-    UPDATING_TESTS_STATUS="TESTS"
-  else
-    UPDATING_TESTS_STATUS="NO_TESTS"
-  fi
-fi
 
-COMMENT_MESSAGE=""
-if [ "$EXISTING_TESTS_STATUS" == "IN_PROGRESS" ]
-then
-  if [ "$UPDATING_TESTS_STATUS" == "IN_PROGRESS" ]
+
+
+
+
+####### Comment logic #######
+
+getAnalysisComment() {
+  COMMENT_ANALYSIS_RUNNING_EXISTING="<h2>:coffee: Diffblue CI is running existing Diffblue tests against your code changes.</h2><ul>"
+  COMMENT_ANALYSIS_UPDATING="<h2>:coffee: Diffblue CI is analysing your code changes to update Diffblue tests.</h2><ul>"
+  COMMENT_ANALYSIS_COMPLETE="<h2>:heavy_check_mark: Diffblue CI has analysed your code changes and is ready for your review.</h2><ul>"
+
+  if [ "$EXISTING_TESTS_STATUS" == "IN_PROGRESS" ]
   then
-    COMMENT_MESSAGE="$COMMENT_ANALYSIS_UPDATING$COMMENT_EXISTING_TODO$COMMENT_UPDATED_TODO"
-  elif [ "$UPDATING_TESTS_STATUS" == "TESTS" ]
+    echo "$COMMENT_ANALYSIS_RUNNING_EXISTING"
+  elif [ "$UPDATING_TESTS_STATUS" == "IN_PROGRESS" ]
   then
-    COMMENT_MESSAGE="$COMMENT_ANALYSIS_UPDATING$COMMENT_EXISTING_TODO$COMMENT_UPDATED_DONE_TESTS"
-  elif [ "$UPDATING_TESTS_STATUS" == "NO_TESTS" ]
+    echo "$COMMENT_ANALYSIS_UPDATING"
+  else
+    echo "$COMMENT_ANALYSIS_COMPLETE"
+  fi
+}
+
+getExistingTestsComment() {
+  COMMENT_EXISTING_TODO="<li>:white_check_mark: Run existing Diffblue tests against your code changes.</li>"
+  COMMENT_EXISTING_DONE_FAILED="<li>:question: Existing Diffblue tests failed against your code changes. See the test report <a href=\"${EXISTING_TESTS_BUILD_URL}testReport/\">here</a> to check if these are expected failures or regressions. Note that any non-compiling tests have been removed and will not appear in the report.</li>"
+  COMMENT_EXISTING_DONE_PASSED="<li>:heavy_check_mark: Existing Diffblue tests passed against your code changes. See the test report <a href=\"${EXISTING_TESTS_BUILD_URL}testReport/\">here</a>. Note that any non-compiling tests have been removed and will not appear in the report.</li>"
+
+  if [ "$EXISTING_TESTS_STATUS" == "IN_PROGRESS" ]
   then
-    COMMENT_MESSAGE="$COMMENT_ANALYSIS_UPDATING$COMMENT_EXISTING_TODO$COMMENT_UPDATED_DONE_NO_TESTS"
-  elif [ "$UPDATING_TESTS_STATUS" == "FAILURE" ]
+    echo "$COMMENT_EXISTING_TODO"
+  elif [ "$EXISTING_TESTS_STATUS" == "SUCCESS" ]
+  then
+    echo "$COMMENT_EXISTING_DONE_PASSED"
+  elif [ "$EXISTING_TESTS_STATUS" == "FAILURE" ] || [ "$EXISTING_TESTS_STATUS" == "UNSTABLE" ]
+  then
+    echo "$COMMENT_EXISTING_DONE_FAILED"
+  else
+    echo "Existing tests status $EXISTING_TESTS_STATUS is not considered in the script."
+    exit 1
+  fi
+}
+
+getUpdatingTestsComment() {
+  COMMENT_UPDATED_TODO="<li>:white_check_mark: Update Diffblue tests to reflect your code changes.</li>"
+  COMMENT_UPDATED_DONE_TESTS="<li>:heavy_check_mark: Diffblue has added commits to update the tests. Check that these reflect the intended behaviour of your code change before merging them. <a href=\"${UPDATING_TESTS_BUILD_URL}console/\">Build log</a></li>"
+  COMMENT_UPDATED_DONE_NO_TESTS="<li>:heavy_check_mark: Diffblue did not update any tests. <a href=\"${UPDATING_TESTS_BUILD_URL}console/\">Build log</a></li>"
+  COMMENT_UPDATED_DONE_FAILURE="<li>:x: Something went wrong. <a href=\"${UPDATING_TESTS_BUILD_URL}console/\">Build log</a></li>"
+
+  GIT_AUTHOR_LIST=$(git log origin/$CHANGE_TARGET..origin/$CHANGE_BRANCH --pretty=%cn)
+  if [ "$UPDATING_TESTS_STATUS" == "COMPLETE" ]
+  then
+    if [[ $GIT_AUTHOR_LIST = *"$(diffblueBotName)"* ]]
     then
-      COMMENT_MESSAGE="$COMMENT_ANALYSIS_UPDATING$COMMENT_EXISTING_TODO$COMMENT_UPDATED_DONE_FAILURE"
-  else
-    echo "Updating tests status $UPDATING_TESTS_STATUS is not considered in the script."
-    exit 1
+      UPDATING_TESTS_STATUS="TESTS"
+    else
+      UPDATING_TESTS_STATUS="NO_TESTS"
+    fi
   fi
-elif [ "$EXISTING_TESTS_STATUS" == "SUCCESS" ]
-then
+
   if [ "$UPDATING_TESTS_STATUS" == "IN_PROGRESS" ]
   then
-    COMMENT_MESSAGE="$COMMENT_ANALYSIS_UPDATING$COMMENT_EXISTING_DONE_PASSED$COMMENT_UPDATED_TODO"
+    echo "$COMMENT_UPDATED_TODO"
   elif [ "$UPDATING_TESTS_STATUS" == "TESTS" ]
   then
-    COMMENT_MESSAGE="$COMMENT_ANALYSIS_COMPLETE$COMMENT_EXISTING_DONE_PASSED$COMMENT_UPDATED_DONE_TESTS"
+    echo "$COMMENT_UPDATED_DONE_TESTS"
   elif [ "$UPDATING_TESTS_STATUS" == "NO_TESTS" ]
   then
-    COMMENT_MESSAGE="$COMMENT_ANALYSIS_COMPLETE$COMMENT_EXISTING_DONE_PASSED$COMMENT_UPDATED_DONE_NO_TESTS"
+    echo "$COMMENT_UPDATED_DONE_NO_TESTS"
   elif [ "$UPDATING_TESTS_STATUS" == "FAILURE" ]
   then
-      COMMENT_MESSAGE="$COMMENT_ANALYSIS_COMPLETE$COMMENT_EXISTING_DONE_PASSED$COMMENT_UPDATED_DONE_FAILURE"
+    echo "$COMMENT_UPDATED_DONE_FAILURE"
   else
     echo "Updating tests status $UPDATING_TESTS_STATUS is not considered in the script."
     exit 1
   fi
-elif [ "$EXISTING_TESTS_STATUS" == "FAILURE" ] || [ "$EXISTING_TESTS_STATUS" == "UNSTABLE" ]
-then
-  if [ "$UPDATING_TESTS_STATUS" == "IN_PROGRESS" ]
-  then
-    COMMENT_MESSAGE="$COMMENT_ANALYSIS_UPDATING$COMMENT_EXISTING_DONE_FAILED$COMMENT_UPDATED_TODO"
-  elif [ "$UPDATING_TESTS_STATUS" == "TESTS" ]
-  then
-    COMMENT_MESSAGE="$COMMENT_ANALYSIS_COMPLETE$COMMENT_EXISTING_DONE_FAILED$COMMENT_UPDATED_DONE_TESTS"
-  elif [ "$UPDATING_TESTS_STATUS" == "NO_TESTS" ]
-  then
-    COMMENT_MESSAGE="$COMMENT_ANALYSIS_COMPLETE$COMMENT_EXISTING_DONE_FAILED$COMMENT_UPDATED_DONE_NO_TESTS"
-  elif [ "$UPDATING_TESTS_STATUS" == "FAILURE" ]
-  then
-    COMMENT_MESSAGE="$COMMENT_ANALYSIS_COMPLETE$COMMENT_EXISTING_DONE_FAILED$COMMENT_UPDATED_DONE_FAILURE"
-  else
-    echo "Updating tests status $UPDATING_TESTS_STATUS is not considered in the script."
-    exit 1
-  fi
-else
-  echo "Existing tests status $EXISTING_TESTS_STATUS is not considered in the script."
-  exit 1
-fi
+}
+
+COMMENT_MESSAGE="$(getAnalysisComment)$(getExistingTestsComment)$(getUpdatingTestsComment)"
+
+
 
 
 ####### Comment API calls #######
